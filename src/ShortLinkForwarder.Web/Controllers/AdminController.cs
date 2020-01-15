201097dd8d51783e0edc9280613ecb4b0f945a43
zengande <zengande@outlook.com>
2020-01-15 10:53:24 +0800

    添加 AdminController

---
+++ src/ShortLinkForwarder.Web/Controllers/AdminController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ShortLinkForwarder.Web.Controllers
+{
+    [Authorize]
+    [Route("admin")]
+    public class AdminController : Controller
+    {
+        [HttpGet, Route("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
